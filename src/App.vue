69fb07d9974d7d99ab2336d6ef6483231033b093
kangansoo <ansoo971@gamail.com>
2024-01-16 13:42:32 +0900

    :tada: Vue로 구구단 게임 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <h1>구구단</h1>
+      <h2>{{ first }}곱하기 {{ second }}는?</h2>
+      <form @submit="onSubmitForm">
+        <!-- input에서 받는 값을 데이터로 연결하기 -> v-model -->
+        <input type="number" ref="answer" v-model="value"/>
+        <button type="submit">입력</button>
+        <!-- <div>{{ value }}</div> <- 이런 식으로 데이터 잘 들어가는지 확인 가능(양방향 바인딩) -->
+      </form>
+      <div>{{ result }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      first: Math.ceil(Math.random() * 9),
+      second: Math.ceil(Math.random() * 9),
+      value: '',
+      result: '',
+    }
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+      console.log('this :', this)
+      console.log('value :', this.value)
+      if (this.first * this.second === parseInt(this.value)){
+        this.result= '정답';
+        this.first = Math.ceil(Math.random() * 9);
+        this.second = Math.ceil(Math.random() * 9);
+        this.value = '';
+        // answer라는 이름의 ref를 선택하여 focus
+        this.$refs.answer.focus();
+      } else {
+        this.result= '땡';
+        this.value = '';
+        this.$refs.answer.focus();
+      }
+    },
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
